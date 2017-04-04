ca8e80159a4bc3bc49870f4e6fa528d84ac4de19
Sergey Gulitsky <sayrex_gs@tut.by>
2017-04-04 11:11:45 +0300

    add proj folder

---
+++ IDL-LVL/Create-FolderShare.ps1
@@ -0,0 +1,30 @@
+﻿#$BackupFolderPath = "C:\Temp"
+#$DatabaseBackupShareName = "ShareName" 
+
+$BackupFolderPath = '#{BackupFolderPath}'
+$DatabaseBackupShareName = '#{DatabaseBackupShareName}' 
+
+if (!(Test-Path $BackupFolderPath)) {
+    New-Item $BackupFolderPath -ItemType Directory -Force -Verbose
+}
+
+if (!(Get-SmbShare $DatabaseBackupShareName)) {
+New-SmbShare -Name $DatabaseBackupShareName -Path $BackupFolderPath `
+                                            -ConcurrentUserLimit "10" `
+                                            -FullAccess 'NT AUTHORITY\SYSTEM' `
+                                            -ChangeAccess 'BUILTIN\Administrators' `
+                                            -ReadAccess 'NT AUTHORITY\Authenticated Users' `
+                                            -NoAccess 'BUILTIN\Guests' `
+                                            -Description 'Created by Octopus step'
+}
+
+else {
+$permissions = @{
+     'NT AUTHORITY\SYSTEM' = 'Full';
+     'BUILTIN\Administrators' = 'Change';
+     'NT AUTHORITY\Authenticated Users' = 'Read';
+     }
+$permissions.GetEnumerator() | ForEach-Object {
+    Grant-SmbShareAccess -Name $DatabaseBackupShareName -AccountName $_.Key -AccessRight $_.Value -Force -Verbose
+    }
+}
