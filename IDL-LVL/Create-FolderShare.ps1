0c2bf3db597412bf3b83ae7515504bb47418841d
Sergey Gulitsky <sayrex_gs@tut.by>
2017-04-04 15:58:26 +0300

    change Create-Share script

--- IDL-LVL/Create-FolderShare.ps1
+++ IDL-LVL/Create-FolderShare.ps1
@@ -1,13 +1,24 @@
-﻿#$BackupFolderPath = "C:\Temp"
-#$DatabaseBackupShareName = "ShareName" 
+﻿$BackupFolderPath = '#{BackupFolderPath}'
+$DatabaseBackupShareName = '#{DatabaseBackupShareName}'
 
-$BackupFolderPath = '#{BackupFolderPath}'
-$DatabaseBackupShareName = '#{DatabaseBackupShareName}' 
+if ([String]::IsNullOrWhiteSpace($SQLServerAccount))
+{
+    Write-Error "The 'SQLServerAccount' variable is not set. The script will not proceed."
+    exit
+}
 
+#Create Folder and set write permissions to SQL Server account
 if (!(Test-Path $BackupFolderPath)) {
+    Write-Host "Creating new directory $BackupFolderPath ..."
     New-Item $BackupFolderPath -ItemType Directory -Force -Verbose
 }
 
+$acl = Get-Acl $BackupFolderPath
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule($SQLServerAccount,"Write, Read", "ContainerInherit, ObjectInherit", "None", "Allow")
+$acl.AddAccessRule($rule)
+Set-Acl $BackupFolderPath $acl
+
+#Create SMB Share and set permissions
 if (!(Get-SmbShare $DatabaseBackupShareName -ErrorAction SilentlyContinue)) {
 New-SmbShare -Name $DatabaseBackupShareName -Path $BackupFolderPath `
                                             -ConcurrentUserLimit "10" `
@@ -16,9 +27,11 @@ New-SmbShare -Name $DatabaseBackupShareName -Path $BackupFolderPath `
                                             -ReadAccess 'NT AUTHORITY\Authenticated Users' `
                                             -NoAccess 'BUILTIN\Guests' `
                                             -Description 'Created by Octopus step'
+Write-Host "New SMB share $DatabaseBackupShareName created."
 }
 
 else {
+Write-Host "SMB share $DatabaseBackupShareName already exists."
 $permissions = @{
      'NT AUTHORITY\SYSTEM' = 'Full';
      'BUILTIN\Administrators' = 'Change';
